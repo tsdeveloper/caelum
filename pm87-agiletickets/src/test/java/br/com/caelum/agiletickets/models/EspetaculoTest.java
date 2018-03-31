c33a7c167c784b55f280bde1911c9e2e985c812d
agil7478 <agil7478@caelumBSB-01-99>
2018-03-31 16:52:33 -0300

    Projeto Inicial

---
+++ pm87-agiletickets/src/test/java/br/com/caelum/agiletickets/models/EspetaculoTest.java
@@ -0,0 +1,101 @@
+package br.com.caelum.agiletickets.models;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
+import junit.framework.Assert;
+
+import org.joda.time.LocalDate;
+import org.joda.time.LocalTime;
+import org.junit.Test;
+
+public class EspetaculoTest {
+
+	@Test
+	public void deveInformarSeEhPossivelReservarAQuantidadeDeIngressosDentroDeQualquerDasSessoes() {
+		Espetaculo ivete = new Espetaculo();
+
+		ivete.getSessoes().add(sessaoComIngressosSobrando(1));
+		ivete.getSessoes().add(sessaoComIngressosSobrando(3));
+		ivete.getSessoes().add(sessaoComIngressosSobrando(2));
+
+		assertTrue(ivete.Vagas(5));
+	}
+
+	@Test
+	public void deveInformarSeEhPossivelReservarAQuantidadeExataDeIngressosDentroDeQualquerDasSessoes() {
+		Espetaculo ivete = new Espetaculo();
+
+		ivete.getSessoes().add(sessaoComIngressosSobrando(1));
+		ivete.getSessoes().add(sessaoComIngressosSobrando(3));
+		ivete.getSessoes().add(sessaoComIngressosSobrando(2));
+
+		assertTrue(ivete.Vagas(6));
+	}
+
+	@Test
+	public void DeveInformarSeNaoEhPossivelReservarAQuantidadeDeIngressosDentroDeQualquerDasSessoes() {
+		Espetaculo ivete = new Espetaculo();
+
+		ivete.getSessoes().add(sessaoComIngressosSobrando(1));
+		ivete.getSessoes().add(sessaoComIngressosSobrando(3));
+		ivete.getSessoes().add(sessaoComIngressosSobrando(2));
+
+		assertFalse(ivete.Vagas(15));
+	}
+
+	@Test
+	public void DeveInformarSeEhPossivelReservarAQuantidadeDeIngressosDentroDeQualquerDasSessoesComUmMinimoPorSessao() {
+		Espetaculo ivete = new Espetaculo();
+
+		ivete.getSessoes().add(sessaoComIngressosSobrando(3));
+		ivete.getSessoes().add(sessaoComIngressosSobrando(3));
+		ivete.getSessoes().add(sessaoComIngressosSobrando(4));
+
+		assertTrue(ivete.Vagas(5, 3));
+	}
+
+	@Test
+	public void DeveInformarSeEhPossivelReservarAQuantidadeExataDeIngressosDentroDeQualquerDasSessoesComUmMinimoPorSessao() {
+		Espetaculo ivete = new Espetaculo();
+
+		ivete.getSessoes().add(sessaoComIngressosSobrando(3));
+		ivete.getSessoes().add(sessaoComIngressosSobrando(3));
+		ivete.getSessoes().add(sessaoComIngressosSobrando(4));
+
+		assertTrue(ivete.Vagas(10, 3));
+	}
+
+	@Test
+	public void DeveInformarSeNaoEhPossivelReservarAQuantidadeDeIngressosDentroDeQualquerDasSessoesComUmMinimoPorSessao() {
+		Espetaculo ivete = new Espetaculo();
+
+		ivete.getSessoes().add(sessaoComIngressosSobrando(2));
+		ivete.getSessoes().add(sessaoComIngressosSobrando(2));
+		ivete.getSessoes().add(sessaoComIngressosSobrando(2));
+
+		assertFalse(ivete.Vagas(5, 3));
+	}
+	@Test
+	public void deveCriarSomenteUmaSessaoQuandoInicioEFimForemIguaisEmSessoesDiarias(){
+		LocalDate inicio = new LocalDate();
+		LocalTime horario = new LocalTime();
+		Espetaculo impeachment = new Espetaculo();
+		
+		List<Sessao> sessoes = impeachment.criaSessoes(inicio, inicio, horario, Periodicidade.DIARIA);
+		
+		Assert.assertEquals(1, sessoes.size());
+	}
+	
+	
+	private Sessao sessaoComIngressosSobrando(int quantidade) {
+		Sessao sessao = new Sessao();
+		sessao.setTotalIngressos(quantidade * 2);
+		sessao.setIngressosReservados(quantidade);
+
+		return sessao;
+	}
+	
+}
